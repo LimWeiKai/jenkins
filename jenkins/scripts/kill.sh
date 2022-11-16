0f801cbb0f2c30269cccf7206e8d1375c6aefb55
LimWeiKai <2001112@sit.singaporetech.edu.sg>
2022-11-16 20:35:16 +0800

    edited for testing UI via Jenkins

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker kill my-apache-php-app
-docker rm my-apache-php-app
+#docker kill my-apache-php-app
+#docker rm my-apache-php-app
+sudo service apache2 stop
+sudo rm /var/www/html/*
